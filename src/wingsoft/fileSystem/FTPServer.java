9386292c6855c40a31bd96eb8684f8c8b2bed90b
qiuyu <q15y5@outlook.com>
2018-11-08 10:18:52 +0800

    1

--- src/wingsoft/fileSystem/FTPServer.java
+++
@@ -1,9 +0,0 @@
-package wingsoft.fileSystem;
-
-public class FTPServer {
-    public static String ftpIP = "192.168.102.5";
-    public static String port = "21";
-    public static String ftpUser ="ftpguest";
-    public static String ftpPwd = "ac89c2a32392d39e";
-    // ftp://ftpguest:ac89c2a32392d39e@192.168.102.5/WF_NYD/pic/21118038.jpg
-}
